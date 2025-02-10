e0a661120f1d0957bd40a6ad3d2473f17e828c45
nnalinn <alina.vysotskayaaa@gmail.com>
2025-02-10 23:04:30 +0200

    make build

---
+++ index.f0404e5f.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in r){var l=document.createElement("li");l.textContent=a,e(l,r[a]),t.appendChild(l)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.f0404e5f.js.map
